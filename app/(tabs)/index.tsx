ec540d99556046cadd1854c34a975cd8d5f4c075
HWANBINYOO <dngh0825@naver.com>
2023-12-13 15:15:30 +0900

    Add : pages

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,31 +1,95 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import { Platform, ScrollView, StyleSheet } from "react-native";
+import Constants from "expo-constants";
+import { Text, View } from "../../components/Themed";
+import { useEffect, useState } from "react";
+import { listProps } from "../../types/list";
+import { fetchList, getFilterList } from "../../api/api";
+import { legnthRn } from "../../util/lengthRn";
+import Header from "../../components/common/Header/Header";
+import { Image } from "expo-image";
+import ListBox from "../../components/ListBox";
 
 export default function TabOneScreen() {
+  const [Initdata, setInitData] = useState<listProps[]>([]);
+  const [list, setList] = useState<listProps[]>([]);
+  const [recommendImg, setRecommendImg] = useState<{
+    imgUrl?: string;
+    imgTitle?: string;
+  }>({});
+
+  const onHandleCategoryClick = async (category: string) => {
+    if (category === "영화" || category === "드라마") {
+      const filterList: listProps[] = await getFilterList(category);
+      setList(filterList);
+    } else setList(Initdata);
+  };
+
+  useEffect(() => {
+    async function initDataFun() {
+      const data: listProps[] = await fetchList();
+      setInitData(data);
+      setList(data);
+      const RN = legnthRn(data);
+      setRecommendImg({
+        imgUrl:
+          data[RN]?.cover?.external?.url ?? data[RN]?.cover?.file?.url ?? "",
+        imgTitle: data[RN]?.properties.Name.title[0].text.content,
+      });
+    }
+    initDataFun();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+    <View style={S.Wrapper}>
+      <Header onCategoryClick={onHandleCategoryClick} />
+      <ScrollView style={S.List} horizontal={false}>
+        <Image
+          source={recommendImg.imgUrl ?? ""}
+          alt="포스터 이미지"
+          style={S.image}
+        />
+        <View style={S.StylegridView}>
+          {list && list.length > 0 ? (
+            list.map((i, index) => (
+              <ListBox
+                key={index}
+                cover={i.cover}
+                title={i.properties.Name.title[0]?.text.content}
+                category={i.properties.Category.multi_select}
+              />
+            ))
+          ) : (
+            <Text>로딩중...</Text>
+          )}
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const S = StyleSheet.create({
+  Wrapper: {
+    width: "100%",
+    height: "100%",
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: "column",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.9)",
+    marginTop: Platform.OS === "ios" ? Constants.statusBarHeight : 0,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  List: {
+    flex: 1,
+    width: "100%",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  StylegridView: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    paddingHorizontal: 16,
+    justifyContent: "space-between",
+    paddingBottom: 80,
+  },
+  image: {
+    width: "100%",
+    height: 200,
   },
 });
